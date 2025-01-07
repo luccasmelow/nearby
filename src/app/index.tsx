faf1ad81d7b98aa6061240393b32b90e8cf8d772
luccasmelow <contatoluccasmelo@gmail.com>
2025-01-07 20:54:50 -0300

    feat: new component in the index and new structured of the layout screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,20 @@
 import { View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+import { router } from "expo-router"
+
 import { Welcome } from "@/components/welcome"
 import { Steps } from "@/components/steps"
-import { SafeAreaView } from "react-native-safe-area-context"
+import { Button } from "@/components/button"
 
 export default function Index(){
     return(
-        <SafeAreaView style={{flex: 1, padding: 24}}>
+        <SafeAreaView style={{flex: 1, paddingHorizontal: 38, gap: 48}}>
             <Welcome/>
             <Steps />
+            <Button onPress={() => router.navigate("/home")}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </SafeAreaView>
     )
 }
